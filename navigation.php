f4f4a89964ebc56f78b1bf3514c295a0a8fd2d16
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2024-01-09 21:14:39 +0100

    vytvořena složky navigation.php a navigation.css

---
+++ navigation.php
@@ -0,0 +1,9 @@
+<nav>
+        <ul>
+            <li><a href="index.php">O mně</a></li>
+            <li><a href="registration-form.php">Skills</a></li>
+            <li><a href="registration-form.php">Vzdělání</a></li>
+            <li><a href="registration-form.php">Portfolio</a></li>
+            <li><a href="registration-form.php">Kontakt</a></li>
+        </ul>
+</nav>
