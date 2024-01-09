350f88c99de181ba07033678b61439e2fab2e0f3
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2024-01-09 21:09:54 +0100

    vytvoření index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/navigation.css">
+    <title>Portfolio Žaneta Sabadková</title>
+</head>
+<body>
+    <header>
+        <h1>Ahoj</h1>
+        <h2>Jmenuji se Žaneta Sabadková</h2>
+        <p>Vítejte na mých stránkách</p>
+    </header>
+    <main>
+        <?php require "navigation.php"; ?>
+
+        <img src="img/my_foto.jpg" alt="">
+
+    </main>
+</body>
+</html>
