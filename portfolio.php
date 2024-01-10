55bd579b3cd3bc7bed003fada5dc4c9c8860649a
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2024-01-10 13:43:50 +0100

    přidána stránka portfolio.php a udělám základ na stránku (header a footer)

---
+++ portfolio.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="css/navigation.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <title>Portfolio</title>
+</head>
+<body>
+    <?php require "assets/navigation.php" ?>
+
+    <main>
+        <a href="https://my-portfolio-one-page.vercel.app/"><img src="" alt=""></a>
+        
+    </main>
+
+    <?php require "assets/footer.php"?>
+    
+</body>
+</html>
