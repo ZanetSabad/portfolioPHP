45db19ee7e5c7a276b909200d916cb63cfaceaaa
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2024-01-12 00:07:17 +0100

    založen contact.php

---
+++ contact.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="css/navigation.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/contact.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php require "assets/navigation.php" ?>
+  
+    <main>
+    <div class="contact">
+                    <div class="phoneNumber"> 
+                        <a href="tel:+420724831189">
+                            <img class= "phone" src="IMG/phone.png" 
+                        alt="telefonní číslo">
+                    </a>
+                    <br>
+                        
+                        <div class="text_number">
+                            <a href="tel:+420724831189">+ 420 724 831 189</a>
+                        </div>
+                    </div>
+
+                    <div class="email"> 
+                        <a href="mailto:zaneta.sabadkova@gmail.com"><img class= "mail" src="IMG/mail.png" 
+                        alt="Email"></a>
+                        
+                        <div class="text_mail"> 
+                            <a href="mailto:zaneta.sabadkova@gmail.com">zaneta.sabadkova@gmail.com</a>
+                        </div>
+                    </div>
+                </div>
+    </main>
+    <?php require "assets/footer.php" ?>
+</body>
+</html>
