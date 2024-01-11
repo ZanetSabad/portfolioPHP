ec2399fffde722a0c1470d3524e03e606ba07bb1
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2024-01-11 22:13:16 +0100

    vytvořen soubor skills.php ajeho základní struktura + vytvořen skills.css a jeho napojení

---
+++ skills.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="css/navigation.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/skills.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php require "assets/navigation.php"?>
+
+    <?php require "assets/footer.php" ?>
+</body>
+</html>
