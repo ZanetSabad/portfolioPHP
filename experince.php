9ae0a6f0a92ffc9a9b9d787881cb81009e5ea693
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2024-01-11 22:31:38 +0100

    vytvořeno experience.css a napojeno na experince.php

--- experince.php
+++ experince.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="css/navigation.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/experince.css"
+    <title>Vzdělání</title>
 </head>
 <body>
     
