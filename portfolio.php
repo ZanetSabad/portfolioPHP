b9a4f667345c57ad08d9f4c0af64c9e5f2dfb2c0
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2024-01-11 22:14:30 +0100

    přidáno section do portfolia

--- portfolio.php
+++ portfolio.php
@@ -13,7 +13,18 @@
     <?php require "assets/navigation.php" ?>
 
     <main>
-        <a href="https://my-portfolio-one-page.vercel.app/"><img src="img/portfolio_mui.png" alt=""></a>
+        <section class="portfolio-card">
+            <a href="https://my-portfolio-one-page.vercel.app/">
+                <img src="img/portfolio_mui.png" alt="portfolio React">
+            </a>
+            <a href="https://contact-form-fawn.vercel.app/">
+                <img src="img/Contact.png" alt="kontaktní formulář">
+            </a>
+            <a href="https://aplikace-ukoly-2wdh.vercel.app/">
+                <img src="img/buy_list.png" alt="aplikace na úkoly">
+            </a>
+        </section>
+       
         
     </main>
 
