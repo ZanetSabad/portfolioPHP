6c9424d14260bd7cf390698c89203f567b49f2b5
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2024-01-12 00:04:34 +0100

    zhotovena stránka životopis

---
+++ experience.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="css/navigation.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/experience.css">
+    <title>Vzdělání</title>
+</head>
+<body>
+    <?php require "assets/navigation.php" ?>
+    <main>        
+             
+               
+              
+                <!-- <div class="download">
+                    <a href="documenty/Zaneta_Sabadkova_CV.pdf" download="Zaneta_Sabadkova_CV.pdf">Stáhnout životopis</a>
+                </div>
+                <div class="media">                    
+                    <a href="https://www.linkedin.com/in/žaneta-sabadková-56542b213/" target="_blank"><img src="IMG/icons/linkedin.png" alt="odkaz na Linkedin"> </a>
+                </div> -->
+                 
+        
+      
+
+        <section class="curiculum col span-3-of-4">
+            <article class="work col span-1-of-2">
+                    <aside class="textCV kurz">
+                        <img class="icon" src="img/icons/kurz.png" alt="ikona absolvovaných kurzů">
+                        <h2>Kurzy</h2>
+                    </aside>
+    
+                    <h3>Attavena, o.p.s. - 2021 / 2022</h3>
+                    <h4>Webová vývojářka</h4>
+                    <p class="popis">Orientace v HTML, CSS, SASS, WordPress, responzivita webu (media queries a flex-box). Základy JavaScriptu. Google Analytics, SEO, Google Search Console. </p>
+    
+                    <figure class="certifications"> 
+                        <a href="documenty/Webová vývojářka.pdf" target="_blank"><img class="document" src="IMG/icons/eyes.png" alt="ikona na otevření certifikát">Vývojářka webu a multimédií</a>
+                    </figure>
+                    <h3>Attavena, o.p.s. - 2021</h3>
+                    <h4>Kurz osobního rozvoje</h4>
+                    <p class="popis">MS Excel, Canva, Zoner Photo Studio X, práce v týmu</p>
+    
+                    <figure class="certifications"> 
+                        <a href="documenty/pracovni_navyky.pdf" target="_blank"><img class="document" src="IMG/icons/eyes.png" alt="oko, prohládnout si certifikát">Pracovní návyky</a>
+                        <a href="documenty/tematicke miniworkshopy.pdf" target="_blank"><img class="document" src="IMG/icons/eyes.png" alt="oko, prohládnout si certifikát">Miniworkshopy</a>
+                        <a href="documenty/orientace_na_trhu_prace.pdf" target="_blank"><img class="document" src="IMG/icons/eyes.png" alt="oko, prohládnout si certifikát">Trh práce</a>
+                    </figure>
+    
+                    <h3>Další certifikáty</h3>
+                    <h4>GOOGLE - Coursera</h4>
+                    <p class="popis">UX Design</p>
+    
+                    <figure class="certifications"> 
+                        <a href="documenty/Coursera.pdf" target="_blank"><img class="document" src="IMG/icons/eyes.png" alt="oko, prohládnout si certifikát">Základy designu UX</a>
+                        <a href="documenty/StartUXDesign.pdf" target="_blank"><img class="document" src="IMG/icons/eyes.png" alt="oko, prohládnout si certifikát">Procesy návrhu UX</a>
+                        <a href="documenty/Wireframes.pdf" target="_blank"><img class="document" src="IMG/icons/eyes.png" alt="oko, prohládnout si certifikát">Výroba wireframů a prototypů Low-Fidelity</a>
+                    </figure>
+                </article>  
+                    
+                <article class="col span-1-of-2 work">
+                    <aside class="textCV">
+                        <img class="icon" src="IMG/icons/stady.png" alt="ikona vzdělávání">
+                        <h2>Vzdělání</h2>
+                    </aside>
+    
+                    <h3>Jihočeská univerzita v Českých Budějovicích</h3>
+                    <h4>Biologie a ochrana zájmových organismů</h4>
+                    <p class="popis">Ukončeno Bc.</p>
+    
+                    <h3>SOŠ veterinární, mechanizační a zahradnická a JŠ s právem státní jazykové zkoušky ČB</h3>
+                    <h4>Veterinární prevence - 2005 / 2009</h4>
+                    
+                    <aside class="textCV">
+                        <img class="icon" src="IMG/icons/work.png" alt="ikona zaměstnání" class="iconWork">
+                        <h2>Pracovní zkušenosti</h2>
+                    </aside>
+                    
+                    <h3>Rodičovská dovolená</h3>
+                    <h4>Manager v domácnosti - 2014 / dosud</h4>
+                    <p class="popis">Schopnost práce pod tlakem, Multitasking, Krizový management, Asertivita, Maximální pracovní výdrž, Komunikační dovednosti, Trvalá práce na sobě</p>
+    
+    
+                    <h3>M RESTAURANT s.r.o.</h3>
+                    <h4>Mc´Donalds - Tábor - 2017 / dosud</h4>
+                    <p class="popis">Schopnost práce pod tlakem, Krizový management, Rychlé učení, Týmová spolupráce</p>
+    
+    
+                    <h3>Manager obchodu</h3>
+                    <h4>Český národní podnik, s.r.o. - 2011 / 2017</h4>
+                    <p class="popis">Bezproblémový chod obchodu, aktivní starost o zákazníky, komunikace, delegování úkolů, práce s financemi, empatie a zpětná vazba</p>
+                </article>
+        </section>
+    </main>
+    <?php require "assets/footer.php" ?>
+</body>
+</html>
