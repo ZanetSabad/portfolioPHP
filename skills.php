294240e3db7d3387edb0067082fc436b449c8b3b
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2024-01-11 22:29:13 +0100

    vytvořeno skills v main

--- skills.php
+++ skills.php
@@ -12,6 +12,100 @@
 <body>
     <?php require "assets/navigation.php"?>
 
+    <main>
+        <section class="skills"> 
+            <article class=" skill">  
+                <div class="cardfull"> 
+                    <div class="card">
+                        <p>HTML</p>
+                    </div>
+                <img src="img/icons/html.png" alt="HTML">
+            </div>
+
+            <div class="cardfull"> 
+                <div class="card">
+                    <p>CSS</p>
+                </div>
+                <img src="img/icons/CSS.png" alt="CSS">
+            </div>
+
+            <div class="cardfull">
+                <div class="card">
+                    <p>JavaScript</p>
+                </div> 
+                <img src="img/icons/JS.png" alt="JavaScript">       
+            </div>
+
+            <div class="cardfull">
+                
+                <div class="card">
+                    <p>SCSS</p>
+                </div> 
+                <img src="img/icons/SCSS.png" alt="SCSS">       
+            </div>
+
+            <div class="cardfull"> 
+                
+                <div class="card">
+                    <p>GIT</p>
+                </div>
+                <img src="img/icons/GIT.png" alt="GIT">        
+            </div>
+
+            <div class="cardfull">
+                
+                <div class="card">
+                    <p>GITHUB</p>
+                </div>
+                <img src="img/icons/GITHUB.png" alt="GitHub">        
+            </div>
+        
+            <div class="cardfull">                    
+                <div class="card">
+                        <p>Python</p>
+                    </div> 
+                    <img src="img/icons/python.png" alt="Python">       
+                </div>
+                
+            <div class="cardfull">    
+                <div class="card">
+                    <p>Google Analytics</p>
+                </div>
+                <img src="img/icons/GA.png" alt="Google Analytics">        
+            </div>
+
+            <div class="cardfull">
+                <div class="card">
+                    <p>SEO</p>
+                </div>
+                <img src="img/icons/SEO.png" alt="SEO">        
+            </div>
+
+            <div class="cardfull"> 
+                <div class="card">
+                    <p>Canva</p>
+                </div>
+                <img src="img/icons/Canva.png" alt="Canva">        
+            </div>
+            <div class="cardfull"> 
+                
+                <div class="card">
+                    <p>ShotCut</p>
+                </div>
+                <img src="img/icons/Shotcut.png" alt="ShotCut">        
+            </div>
+            <div class="cardfull">
+                
+                <div class="card">
+                    <p>Figma</p>
+                </div>        
+                <img src="img/icons/Figma.png" alt="Figma">
+            </div>
+        
+    </article>
+    </section>
+    </main>
+
     <?php require "assets/footer.php" ?>
 </body>
 </html>
